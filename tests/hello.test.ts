dd5b8308c2f25db72ab11afbaa319f84e0dab810
Ka <k.ardliyan@gmail.com>
2023-07-13 09:08:18 +0700

    a: setup typescript project

---
+++ tests/hello.test.ts
@@ -0,0 +1,7 @@
+describe('Hello', function () {
+    it('should say Hello', function () {
+        const name = 'Harifa';
+        const result = `Hello ${name}`;
+        expect(result).toBe('Hello Harifa');
+    });
+});
